a83b920469f215e02e921169e535eea7d02e5057
Niels Gregers Johansen <niels@timeboxer.dk>
2023-04-15 08:23:31 +0000

    Add Powershell module installation script

---
+++ scripts/install.ps1
@@ -0,0 +1,3 @@
+Install-Module -Name ExchangeOnlineManagement -Force -Verbose
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force -Verbose
+Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -Force -Verbose
