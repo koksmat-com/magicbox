b91d55fee1baf0c6332c123157253134169e6670
Niels Gregers Johansen <niels@jumpto365.com>
2023-04-22 17:40:34 +0200

    Fixed bug

--- apps/workers/src/Worker.ts
+++ apps/workers/src/Worker.ts
@@ -26,7 +26,7 @@ export class Worker {
         if (!message.route){
           return {hasError:true,errorMessage:"No route specified"}
         }
-        const result = await Factory.getInstance().processMessage(message.method,message.route,message.payload)
+        const result = await Factory.getInstance().processMessage(message.method,message.route,message)
        
         return result
       });
