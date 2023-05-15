ded9c8946a3291ba6025135d9ac9d21d86ea4d6e
Karlo Mrakovcic <karlomrak@gmail.com>
2023-05-15 10:23:02 +0200

    Updated leftnav

--- apps/web/src/app/booking/layout.tsx
+++ apps/web/src/app/booking/layout.tsx
@@ -1,26 +1,21 @@
-"use client"
-import LeftNav from "./leftnav"
-import { FluentProvider, webLightTheme,webDarkTheme } from '@fluentui/react-components';
+"use client";
+import LeftNav from "./leftnav";
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+} from "@fluentui/react-components";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    //TODO: Bright/Dark theme changer
     <FluentProvider theme={webDarkTheme}>
-    <div className="flex">
-      <div className="w-96">
-      <LeftNav/>
+      <div className="flex">
+        <div className="w-96">
+          <LeftNav />
+        </div>
+        <div>{children}</div>
       </div>
-      <div>
-      {children}
-      </div>
-    </div>
     </FluentProvider>
-
-
-
-        
-  )
+  );
 }
--- apps/web/src/app/booking/leftnav.tsx
+++ apps/web/src/app/booking/leftnav.tsx
@@ -1,5 +1,10 @@
-export default function LeftNav(){
-    return <div>
-        Left
+export default function LeftNav() {
+  return (
+    <div>
+      <div>Home</div>
+      <div>My bookings</div>
+      <div>Settings</div>
+      <div>Admin</div>
     </div>
+  );
 }
