1e0915d0a4ea89197075c6d29bc0cb1977a6ae97
Karlo Mrakovcic <karlomrak@gmail.com>
2023-07-04 14:00:38 +0200

    added BUs

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,28 +1,30 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { TopBar } from '@/components-lowlevel/TopBar'
-import nexiEurope from "./NexiEurope.svg"
-import { NavigationNode, NexiNavConfig } from '@/components-lowlevel/helpers'
-import { ITopNavigation, TopNavigation } from '@/components-lowlevel/components/Topnav'
-import Link from 'next/link'
-import Logo from '@/components-lowlevel/components/Logo'
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { TopBar } from "@/components-lowlevel/TopBar";
+import nexiEurope from "./NexiEurope.svg";
+import { NavigationNode, NexiNavConfig } from "@/components-lowlevel/helpers";
+import {
+  ITopNavigation,
+  TopNavigation,
+} from "@/components-lowlevel/components/Topnav";
+import Link from "next/link";
+import Logo from "@/components-lowlevel/components/Logo";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const nav: NavigationNode[] = []
+  const nav: NavigationNode[] = [];
   const hubConfig: NexiNavConfig = {
     enabled: false,
     parents: [],
-    clarityId: '',
-    matomoId: '',
+    clarityId: "",
+    matomoId: "",
     showSearch: false,
     hideHome: false,
-    homeUrl: '',
+    homeUrl: "",
     showSiteTitle: false,
-    siteUrl: '',
-    siteTitle: ''
-  }
-
+    siteUrl: "",
+    siteTitle: "",
+  };
 
   const left: NavigationNode[] = [
     {
@@ -30,40 +32,37 @@ export default function Home() {
       Url: "/",
       Children: [],
       onOver: (node: NavigationNode) => {
-        console.log("Mouse over home")
+        console.log("Mouse over home");
       },
       onOut: (node: NavigationNode) => {
-        console.log("Mouse out home")
-      }
+        console.log("Mouse out home");
+      },
     },
-
-  ]
+  ];
   const topNavigationProps: ITopNavigation = {
-
     left: nav,
     right: nav,
 
     hubConfig,
-    homeUrl: "/"
-
-  }
+    homeUrl: "/",
+  };
   return (
     <div className="container  text-center bg-[url('/NexiEurope.svg')] bg-repeat-space h-screen bg-contain bg-center">
-      <div className='absolute left-8 top-4'>
-      <Logo homeUrl='/' />
+      <div className="absolute left-8 top-4">
+        <Logo homeUrl="/" />
       </div>
       {/* <TopNavigation {...topNavigationProps} /> */}
-      <div className='grid h-screen place-items-center'>
-        <div className=' w-screen p-10 bg-[#FFFFFFAA]'>
-          <div className='pb-4 text-2xl'>Welcome to Nexi Group</div>
+      <div className="grid h-screen place-items-center">
+        <div className=" w-screen p-10 bg-[#FFFFFFAA]">
+          <div className="pb-4 text-2xl text-black">Welcome to Nexi Group</div>
           <div>
-          <button  className="rounded-full text-white bg-[#2D32A9] p-2 px-10 hover:from-pink-500 hover:to-yellow-500 from-green-400 to-blue-500"> <Link href="/welcome">Click to get started</Link></button>
-            
-           </div>
+            <button className="rounded-full text-white bg-[#2D32A9] p-2 px-10 hover:from-pink-500 hover:to-yellow-500 from-green-400 to-blue-500">
+              {" "}
+              <Link href="/welcome">Click to get started</Link>
+            </button>
+          </div>
         </div>
-
       </div>
-
     </div>
-  )
+  );
 }
--- apps/web/src/app/welcome/page.tsx
+++ apps/web/src/app/welcome/page.tsx
@@ -1,38 +1,69 @@
-
-
-import Layout from "@/components-highlevel/Layout"
+import Layout from "@/components-highlevel/Layout";
 import createClient from "openapi-fetch";
 import { paths } from "./data/schemas/magicbox.nets-intranets"; // (generated from openapi-typescript)
 import IntranetCountry from "./showcountry";
 
-
-
 export default async function Intranet() {
-
-
-    const { get } = createClient<paths>({
-        baseUrl: "https://magicbox.nets-intranets.com",
-        // headers: {
-        //   Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
-        // },
-    });
-
-    const { data, error } = await get("/v1/business/countries", { cache: "no-cache"});
-
-
-    if (error) {
-
-        return <div>{error.code}</div>
-    }
-    return <div>
-
-
-        <Layout>
-            Hello world
-            {data?.countries?.map((country, id) => {
-                return <IntranetCountry  key={id} name={country.Name} code="cc" />
-            })}
-        </Layout>
-
+  const countries = await Countries();
+  const units = await Units();
+  return (
+    <div>
+      <div>{countries}</div>
+      <div>{units}</div>
     </div>
+  );
+}
+
+export async function Countries() {
+  const { get } = createClient<paths>({
+    baseUrl: "https://magicbox.nets-intranets.com",
+    // headers: {
+    //   Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    // },
+  });
+
+  const { data, error } = await get("/v1/business/countries", {
+    cache: "no-cache",
+  });
+
+  if (error) {
+    return <div>{error.code}</div>;
+  }
+  return (
+    <div>
+      <Layout>
+        Hello world
+        {data?.countries?.map((country, id) => {
+          return <IntranetCountry key={id} name={country.Name} code="cc" />;
+        })}
+      </Layout>
+    </div>
+  );
+}
+
+export async function Units() {
+  const { get } = createClient<paths>({
+    baseUrl: "https://magicbox.nets-intranets.com",
+    // headers: {
+    //   Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+    // },
+  });
+
+  const { data, error } = await get("/v1/business/units", {
+    cache: "no-cache",
+  });
+
+  if (error) {
+    return <div>{error.code}</div>;
+  }
+  return (
+    <div>
+      <Layout>
+        Units
+        {data?.units?.map((unit, id) => {
+          return <IntranetCountry key={id} name={unit.name} code="cc" />;
+        })}
+      </Layout>
+    </div>
+  );
 }
